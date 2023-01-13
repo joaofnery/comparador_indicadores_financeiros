b51222607afb4564d45b5a1bc411b0afcc689cfd
João Fabrício Nery <121911602+joaofnery@users.noreply.github.com>
2023-01-12 21:42:12 -0300

    Add files via upload

---
+++ comparador_indicadores_financeiros.py
@@ -0,0 +1,5 @@
+naçoes = int(input('Qual a quantidade de Ações/FIIs deseja comparar? '))
+ind1 = str(input('Qual será o primeiro indicador? ')).strip()
+ind2 = str(input('Qual será o segundo indicador? ')).strip()
+ind3 = str(input('Qual será o terceiro indicador? ')).strip()
+
