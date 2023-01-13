885d1390f7333247f4c42029e9a084f37b472510
João Fabrício Nery <121911602+joaofnery@users.noreply.github.com>
2023-01-12 21:53:37 -0300

    Add files via upload

--- comparador_indicadores_financeiros.py
+++ comparador_indicadores_financeiros.py
@@ -2,4 +2,13 @@ naçoes = int(input('Qual a quantidade de Ações/FIIs deseja comparar? '))
 ind1 = str(input('Qual será o primeiro indicador? ')).strip()
 ind2 = str(input('Qual será o segundo indicador? ')).strip()
 ind3 = str(input('Qual será o terceiro indicador? ')).strip()
-
+acao( = 0
+valor1 = 0
+valor2 = 0
+valor3 = 0
+for n in range(1, naçoes + 1):
+    acao[n] = str(input('Informe a Ação/FII [{}]: '.format(n)))
+    valor1[n] = float(input('Qual o {}, de {}.'.format(ind1, acao[n])))
+    valor2[n] = float(input('Qual o {}, de {}. '.format(ind2, acao[n])))
+    valor3[n] = float(input('Qual o {}, de {}. '.format(ind3, acao[n])))
+    print(acao, valor1, valor2, valor3)
