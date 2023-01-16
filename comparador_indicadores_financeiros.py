211279ef13b872bb71ac281fea754c6a4f325a0e
João Fabrício Nery <121911602+joaofnery@users.noreply.github.com>
2023-01-16 00:22:34 -0300

    Add files via upload

--- comparador_indicadores_financeiros.py
+++ comparador_indicadores_financeiros.py
@@ -109,17 +109,17 @@ elif nacoes == 3:
         media1 += 2
         media2 += 1
 
-    if listavalor2[0] < listavalor2[1] < listavalor2[2]:
+    if listavalor3[0] < listavalor3[1] < listavalor3[2]:
         media0 += 1
         media1 += 2
         media2 += 3
 
-    elif listavalor2[0] < listavalor2[2] < listavalor2[1]:
+    elif listavalor3[0] < listavalor3[2] < listavalor3[1]:
         media0 += 1
         media1 += 3
         media2 += 2
 
-    elif listavalor2[1] < listavalor2[0] < listavalor2[2]:
+    elif listavalor3[1] < listavalor3[0] < listavalor3[2]:
         media0 += 2
         media1 += 1
         media2 += 3
@@ -138,3 +138,21 @@ elif nacoes == 3:
         media0 += 3
         media1 += 2
         media2 += 1
+
+    if media0 < media1 < media2:
+        print('[1] {}         [2] {}        [3] {}'.format(listaacao[0], listaacao[1], listaacao[2]))
+        
+    elif media0 < media2 < media1:
+        print('[1] {}         [2] {}        [3] {}'.format(listaacao[0], listaacao[2], listaacao[1]))
+
+    elif media1 < media0 < media2:
+        print('[1] {}         [2] {}        [3] {}'.format(listaacao[1], listaacao[0], listaacao[2]))
+
+    elif media1 < media2 < media0:
+        print('[1] {}         [2] {}        [3] {}'.format(listaacao[1], listaacao[2], listaacao[0]))
+
+    elif media2 < media0 < media1:
+        print('[1] {}         [2] {}        [3] {}'.format(listaacao[2], listaacao[0], listaacao[1]))
+
+    elif media2 < media1 < media0:
+        print('[1] {}         [2] {}        [3] {}'.format(listaacao[2], listaacao[1], listaacao(0)))
